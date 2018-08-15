ecc3f9264071c96b1b17dbf16309fb486327f5fc
davified <davified@gmail.com>
2018-08-15 17:15:23 +0800

    Create project from template

---
+++ typography.js
@@ -0,0 +1,17 @@
+import Typography from 'typography'
+
+const typography = new Typography({
+  baseFontSize: '18px',
+  baseLineHeight: 1.45,
+  headerFontFamily: [
+    'Avenir Next',
+    'Helvetica Neue',
+    'Segoe UI',
+    'Helvetica',
+    'Arial',
+    'sans-serif',
+  ],
+  bodyFontFamily: ['Arial', 'serif'],
+})
+
+export default typography
